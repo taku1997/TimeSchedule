84bfa7a0d03634d54200bef547745b82204d1fb7
taku1010 <takumi.guitar1010@gmail.com>
2021-03-23 21:06:35 +0900

    heroku 修正

--- app.js
+++ app.js
@@ -104,7 +104,6 @@ cron.schedule('59 59 23 * * *',() => {
   });
 })
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -115,7 +114,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
